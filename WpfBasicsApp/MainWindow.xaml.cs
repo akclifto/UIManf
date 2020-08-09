d6741469af193a170038190557f3612a1ee6f57e
akclifto <akclifto@asu.edu>
2020-08-09 15:42:42 -0700

    funcitoning reset field, apply temp set

--- WpfBasicsApp/MainWindow.xaml.cs
+++ WpfBasicsApp/MainWindow.xaml.cs
@@ -34,13 +34,33 @@ namespace WpfBasicsApp
         {
             //example:  sets all checked boxes from work centers to flase (unchecked)
             this.WeldChBox.IsChecked = this.AssemblyChBox.IsChecked = this.PlasmaChBox.IsChecked = this.LaserChBox.IsChecked =
-                this.PurchaseChBox.IsChecked = this.LatheChBox.IsChecked = this.DrillChBox.IsChecked = this.FoldChBox.IsChecked = 
+                this.PurchaseChBox.IsChecked = this.LatheChBox.IsChecked = this.DrillChBox.IsChecked = this.FoldChBox.IsChecked =
                 this.RollChBox.IsChecked = this.SawChBox.IsChecked = false;
+            //clear the description test box
+            this.DescrTextBox.Clear();
+            //clear the materials dropdown, length, mass, set defaults to finish, purchase info, supplier name/code/note
+            this.MaterialDropDown.SelectedIndex = 0;
+            this.LengthTextBox.Clear();
+            this.MassTextBox.Clear();
+            this.FinishDropDown.SelectedIndex = 0;
+            this.PurchaseInfoDropDown.SelectedIndex = 0;
+            this.SupplierNameTextBox.Clear();
+            this.SupplierCodeTextBox.Clear();
+            this.NoteTextBox.Clear();
+            Debug("All fields reset  in UI");
         }
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Placeholder:  Refresh Needs functionality");
+        }
+
+        private void Debug(String message)
+        {
+            //this is called an interpolated string.  Basically the message from debug gets passed in the { }.
+            //      format:
+            //          $ + some text + {message being passed as a parameter}
+            System.Diagnostics.Debug.WriteLine($"Debug: {message}");
         }
     }
 }
