503ce62343ffe696d0bb89d7e7f74f2b833fc326
akclifto <akclifto@asu.edu>
2020-08-09 15:59:39 -0700

    update apply, reset and refresh fields, add debug

--- WpfBasicsApp/MainWindow.xaml.cs
+++ WpfBasicsApp/MainWindow.xaml.cs
@@ -27,9 +27,15 @@ namespace WpfBasicsApp
 
         private void ApplyButton_Click(object sender, RoutedEventArgs e)
         {
+            Debug("ApplyButton_Clicked and description field accessed.");
             MessageBox.Show($"The description of the item is: \n\n{this.DescrTextBox.Text}");
         }
 
+        /// <summary>
+        /// ResetButton method to reset all the fields in the UI. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
             //example:  sets all checked boxes from work centers to flase (unchecked)
@@ -52,9 +58,23 @@ namespace WpfBasicsApp
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e)
         {
+            Debug("Refresh Button Pressed!");
             MessageBox.Show("Placeholder:  Refresh Needs functionality");
         }
 
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            //generic adding to field just to make sure we can get some input we want in the field. 
+            this.LengthTextBox.Text += ((CheckBox)sender).Content + " ";
+            Debug("CheckBox Checked function accessed and populated lengthTextBox field.");
+        }
+
+
+        /// <summary>
+        /// Debug for testing purposes.  Writes out to console with a message.
+        /// </summary>
+        /// <param name="message">  Message to pass through the debugger. </param> 
         private void Debug(String message)
         {
             //this is called an interpolated string.  Basically the message from debug gets passed in the { }.
