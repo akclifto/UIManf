7ecb0a2a0d96fb1b1431fb01247eb35c49a91ee4
akclifto <akclifto@asu.edu>
2020-08-10 17:12:29 -0700

    tree expansion logic complete

--- WpfBasicsApp/MainWindow.xaml.cs
+++ WpfBasicsApp/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace WpfBasicsApp
         }
         #endregion
 
-        #region Left Side of Panel: On loaded Tree FolderView
+        #region Left Side of Panel: On loaded
         /// <summary>
         /// Initialization of application.
         /// </summary>
@@ -60,10 +60,17 @@ namespace WpfBasicsApp
                 FolderView.Items.Add(item);
             }
         }
+        #endregion On Loaded
 
-
+        #region Folder Expanded Method
+        /// <summary>
+        /// Expands the tree, gets all subitems in the tree and populates left side of panel. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Folder_Expanded(object sender, RoutedEventArgs e)
         {
+            #region Folder_Expanded: Initial Checks
             //get the tree view item, root item (parent)
             var item = (TreeViewItem)sender;
             //check for dummy data:  if the item count is not 1 or it is and the item is not null
@@ -74,8 +81,11 @@ namespace WpfBasicsApp
 
             //clear dummy data 
             item.Items.Clear();
-
             var fullPath = (string) item.Tag;
+
+            #endregion Folder_Expanded: Initial Checks
+
+            #region Folder_Expanded: Get Folders
 
             //create list to hold directories
             var directories = new List<string>();
@@ -91,8 +101,9 @@ namespace WpfBasicsApp
                 }
 
             } catch {
-                Debug("Caught an error when populating directories!");
+                Debug("Caught an error when getting directories!");
             }
+
 
             //For each directory, add to the folder view tree
             directories.ForEach(directoryPath =>
@@ -117,41 +128,83 @@ namespace WpfBasicsApp
                 //Debug("Subitem added to folderview");
 
             });
+            #endregion Folder_Expanded:  Get Folders
+
+            #region Folder_Expanded: Get Files
+
+            //create list to hold files
+            var files = new List<string>();
+
+            //try/catch to check each drive to make sure something is there to populate the tree.  Bad practice to not handle errors. 
+            //get files and add to list
+            try
+            {
+                var fileDirs = Directory.GetFiles(fullPath);
+                if (fileDirs.Length > 0)
+                {
+                    files.AddRange(fileDirs);
+                }
+
+            }
+            catch
+            {
+                Debug("Caught an error when getting file names!");
+            }
+
+            #region Folder_Expanded: Get Folders
+
+            //For each directory, add to the folder view tree
+            files.ForEach(filepath =>
+            {
+                //create file item
+                var subItem = new TreeViewItem()
+                {
+                    //set the header as the file name
+                    Header = GetFileFolderName(filepath),
+                    //set tag as full path name
+                    Tag = filepath
+                };
+
+                //add child subitem to parent
+                item.Items.Add(subItem);
+                //Debug("Subitem added to folderview");
+
+            });
+
+            #endregion Folder_Expanded: Get Files
 
         }
+        #endregion Folder_Expanded
+        #endregion Left Side of Panel
 
-
-
-        #endregion
-
-        #region helper methods: public
+        #region Helper Methods:  GetFileFolderName
         /// <summary>
         /// Helper method to find the file or folder name from a full path. 
         /// </summary>
-        /// <param name="directoryPath">The full path name</param>
+        /// <param name="filepath">The full path name</param>
         /// <returns>Returns just the last part of the filepath name (C:\Some Folder\file.png would return "file.png")</returns>
-        private object GetFileFolderName(string directoryPath)
+        private string GetFileFolderName(string filepath)
         {
             //if no path, return empty., 
-            if (string.IsNullOrEmpty(directoryPath))
+            if (string.IsNullOrEmpty(filepath))
             {
                 Debug("File Folder Name path is empty, returning empty");
                 return string.Empty;
             }
 
             //make all slashes backslashes
-            var normalizedPath = directoryPath.Replace('/', '\\');
+            var normalizedPath = filepath.Replace('/', '\\');
             //find the last backslash in the path. 
             var lastIndex = normalizedPath.LastIndexOf('\\');
 
             // If only root directory, return the path itself (ex, file.png)
             if(lastIndex <= 0)
             {
-                return directoryPath;
+                return filepath;
             }
 
             //return the string name after the last backslash.
-            return directoryPath.Substring(lastIndex + 1);
+            return filepath.Substring(lastIndex + 1);
 
         }
 
