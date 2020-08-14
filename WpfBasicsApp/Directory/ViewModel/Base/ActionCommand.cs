f2b218c63f6b012f68e95765a13ca05022edd1a8
akclifto <akclifto@asu.edu>
2020-08-14 15:35:57 -0700

    building dirstruct viewmodel

---
+++ WpfBasicsApp/Directory/ViewModel/Base/ActionCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfBasicsApp
+{
+    /// <summary>
+    /// This class handles a command that runs an action, such as a button press, tree exapnsion, etc.
+    /// </summary>
+    public class ActionCommand : ICommand
+    {
+        #region Private Attributes
+
+        /// <summary>
+        /// Action to run.
+        /// </summary>
+        private Action mAction;
+
+        #endregion Private Attributes
+
+        #region Public Events
+
+        /// <summary>
+        /// The event that executes when the <see cref="CanExecute(object)"/> value has changed.
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        #endregion Public Events
+
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public ActionCommand(Action action)
+        {
+            mAction = action;
+        }
+
+        #endregion Constructor
+
+        #region Command Methods
+
+        /// <summary>
+        /// Action command will always execute.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns>true</returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the command action being passed.
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            mAction();
+        }
+
+        #endregion Command Methods
+    }
+}
---
+++ WpfBasicsApp/Directory/ViewModel/DirectoryStructureViewModel.cs
@@ -0,0 +1,7 @@
+﻿
+namespace WpfBasicsApp
+{
+    class DirectoryStructureViewModel
+    {
+    }
+}
